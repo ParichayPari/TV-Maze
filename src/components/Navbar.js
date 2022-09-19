abefab7104e7927de427e564f3b39efd58d6a2f9
ParichayPari <107365891+ParichayPari@users.noreply.github.com>
2022-09-19 11:44:07 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function NavBar() {
+function Navbar() {
   return (
     <>
       <section>
@@ -18,4 +18,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default Navbar;
